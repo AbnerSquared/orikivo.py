921025e677c7525802cefe79827ae156c8200647
Orikingium Studios <25711694+AbnerSquared@users.noreply.github.com>
2017-09-30 23:52:58 -0500

    Update musiccore.py

--- musiccore.py
+++ musiccore.py
@@ -3,12 +3,11 @@ import discord
 from discord.ext import commands
 
 if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
     discord.opus.load_opus('opus')
+
+    
+# I know it's basing off of the playlist example. This is the original version, and a revised one with better usage is in the works.
+# And yes, I did type the entire example by scratch, despite it may look the exact same. :(
 
 class VoiceEntry:
     def __init__(self, message, player):
