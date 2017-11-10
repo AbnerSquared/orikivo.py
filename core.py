431595e718d43bf7717956f90aed77aea9bbd082
Orikingium Studios <25711694+AbnerSquared@users.noreply.github.com>
2017-11-10 14:36:37 -0600

    Core beginnings.

---
+++ core.py
@@ -0,0 +1,7 @@
+import discord
+from discord.ext import commands
+
+onCMDPfx = "/"
+onDesc = "The core systematic of Orikivo. More is yet to come."
+
+ori = discord.ext.commands.Bot(command_prefix = onCMDPfx, description = onDesc)
